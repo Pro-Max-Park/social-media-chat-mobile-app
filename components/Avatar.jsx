de6fe9f3ec84b1f52043afd4f8f06b9d10dcd1e2
Pro-Max-Park <peterpark2000@hotmail.com>
2026-06-08 15:41:13 +1000

    Initial commit: Social Media Chat Mobile App

---
+++ components/Avatar.jsx
@@ -0,0 +1,36 @@
+import { Image } from "expo-image";
+import { StyleSheet, View } from "react-native";
+import { theme } from "../constants/theme";
+import { hp } from "../helpers/common";
+import { getUserImageSrc } from "../services/imageService";
+
+const Avatar = ({
+  uri,
+  size = hp(4.5),
+  rounded = theme.radius.md,
+  style = {},
+}) => {
+  return (
+    <View>
+      <Image
+        source={getUserImageSrc(uri)}
+        transition={100}
+        style={[
+          styles.avatar,
+          { height: size, width: size, borderRadius: rounded },
+          style,
+        ]}
+      />
+    </View>
+  );
+};
+
+export default Avatar;
+
+const styles = StyleSheet.create({
+  avatar: {
+    borderCurve: "continuous",
+    borderColor: theme.colors.darkLight,
+    borderWidth: 1,
+  },
+});
---
+++ constants/index.js
@@ -0,0 +1,2 @@
+export const supabaseUrl = 'https://vhzslkoofrxzjvdrzzib.supabase.co';
+export const supabasePublishableKey = 'sb_publishable_3SqxQ0SHbexEw99tvESgbA_M-WwuPY_';
